e6afc30ca4a7adf023c144a9e4ebbd18a4ad1584
Rivo Laks <code@rivolaks.com>
2016-11-05 15:44:08 +0100

    Fix tests on Django < 1.8

--- demo/settings.py
+++ demo/settings.py
@@ -3,6 +3,8 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 ROOT_URLCONF = 'demo.urls'
 INSTALLED_APPS = ['demo']
+
+# TEMPLATES is used by Django >= 1.8. TEMPLATE_CONTEXT_PROCESSORS below is for compatibility with older versions.
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -15,6 +17,8 @@ TEMPLATES = [
         },
     },
 ]
+TEMPLATE_CONTEXT_PROCESSORS = ['django_settings_export.settings_export']
+
 DATABASES = {'default': {'NAME': 'db.sqlite',
                          'ENGINE': 'django.db.backends.sqlite3'}}
 
