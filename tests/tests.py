7a462a6039c7a72e080aea94957ee3795517d9bb
Jakub Roztocil <jakub@roztocil.co>
2016-11-06 12:08:35 +0100

    More tests

--- tests/tests.py
+++ tests/tests.py
@@ -16,9 +16,35 @@ class TestExportedSettings(TestCase):
     def test_key_access_ok(self):
         self.assertEqual(self.settings['FOO'], 'BAR')
 
-    def test_key_access_unexported(self):
+    def test_key_access_UnexportedSettingError(self):
         with self.assertRaises(UnexportedSettingError):
             self.settings['XXX']
+
+    def test_key_access_dict_method(self):
+        """
+        Since `items()` is an existing `dict` method, a KeyError needs to
+        be raised instead of UnexportedSettingError, so that Django templates
+        re-try with attribute access.
+
+        Django first tries dict lookup and relies on specific exceptions being
+        raised if the key is absent from the dict (specifically one of
+        TypeError, AttributeError, KeyError, ValueError, IndexError).
+
+        https://github.com/jkbrzt/django-settings-export/pull/4
+
+        """
+        with self.assertRaises(KeyError):
+            self.settings['items']
+
+    def test_key_access_dict_can_assign(self):
+        """"
+        When there's is a setting name colliding with a `dict` method,
+        things still should work. The ability to access `items() form
+        templates, is lost though, which is acceptable.
+
+        """
+        self.settings['items'] = 'value'
+        self.assertEqual(self.settings['items'], 'value')
 
     def test_dict_keys(self):
         self.assertListEqual(list(self.settings.keys()), ['FOO'])
